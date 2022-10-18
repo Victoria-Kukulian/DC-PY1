31b624471d12ef462e983715158eb062401352dd
Victoria <115472673+Victoria-Kukulian@users.noreply.github.com>
2022-10-18 14:18:29 +0300

    Лабораторная работа 3

---
+++ Лабораторная работа 3/task1.py
@@ -0,0 +1,10 @@
+src = not False and True or False and not True
+
+result = True
+
+print(src == result)
+
+# result = True and True or False and False # избавляемся от отрицаний
+# result = True or False  # избавляемся от логического "И"
+# result = True  # избавляемся от логического "ИЛИ"
+
