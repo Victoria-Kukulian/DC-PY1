4303024bf3a72e43bcfc9633825ed457e67158fb
Victoria <115472673+Victoria-Kukulian@users.noreply.github.com>
2022-11-13 21:35:44 +0300

    Add files via upload

---
+++ Лабораторная работа 5/task3.py
@@ -0,0 +1,13 @@
+from random import randint
+
+def get_unique_list_numbers(start = -10, stop = 10, count = 15) -> list[int]:
+    list_ = []
+    while len(list_) < count:
+        new_number = randint(start, stop)
+        if new_number not in list_:
+            list_.append(new_number)
+    return list_
+
+list_unique_numbers = get_unique_list_numbers()
+print(list_unique_numbers)
+print(len(list_unique_numbers) == len(set(list_unique_numbers)))
